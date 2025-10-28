b8c4dfc1f6f4f8bedea266251b4d5cb94588c716
Tosi-eu <galdiniguilherme@usp.br>
2025-10-28 09:33:53 -0300

    feat: table to cabinet stock

--- mocks/cabinets.ts
+++ mocks/cabinets.ts
@@ -5,26 +5,21 @@ export const cabinets: Cabinet[] = [
   {
     id: 1,
     category: CabinetCategory.PSICOTROPICOS_E_INJECOES,
-    description: "Armário de medicamentos controlados",
   },
   {
     id: 2,
     category: CabinetCategory.MEDICAMENTOS_DOADOS,
-    description: "Medicamentos doados mês passado",
   },
   {
     id: 3,
     category: CabinetCategory.DIVERSOS,
-    description: "Documentos administrativos",
   },
   {
     id: 4,
     category: CabinetCategory.MEDICACAO_GERAL,
-    description: "Medicamentos de uso geral",
   },
   {
     id: 5,
     category: CabinetCategory.DIVERSOS,
-    description: "Outros materiais e suprimentos diversos",
   },
 ];
